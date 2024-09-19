b2244e1bb00933c541b3cc1dab5f6942ec5ddb25
nbellila <nbellila@student.42.fr>
2024-09-19 20:25:59 +0200

    print contact table

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -1,10 +1,12 @@
 #include "PhoneBook.hpp"
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 PhoneBook::PhoneBook(void)
 {
     this->count = 0;
+    this->col_width = 10;
 }
 
 void    PhoneBook::showInputs(void)
@@ -21,11 +23,31 @@ void    PhoneBook::addContact(Contact contact)
     this->contacts[0] = contact;
 }
 
-void    PhoneBook::showContacts(void)
+void    PhoneBook::printColumn(std::string str, bool sep)
 {
+    if (str.size() > this->col_width)
+        std::cout << str.substr(0, this->col_width - 1) << ".";
+    else
+        std::cout << std::setfill(' ') << std::setw(this->col_width) << str;
+    if (sep)
+        std::cout << "|";
+}
+
+void    PhoneBook::printContacts(void)
+{
+    std::cout << std::endl << std::endl;
+    this->printColumn("Index", true);
+    this->printColumn("First name", true);
+    this->printColumn("Last name", true);
+    this->printColumn("Nickname", false);
+    std::cout << std::endl << std::endl;
     for (int i = 0; i < 8; i++)
     {
-        std::cout << "-----------\n";
-        this->contacts[i].showData();    
+        std::cout << std::setfill(' ') << std::setw(this->col_width) << i << "|";
+        this->printColumn(this->contacts[i].first_name, true);
+        this->printColumn(this->contacts[i].last_name, true);
+        this->printColumn(this->contacts[i].nickname, false);
+        std::cout << std::endl;
     }
+    std::cout << std::endl;
 }
