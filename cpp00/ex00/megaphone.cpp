ad4d6b9ed321a72e2da7cebeb40f73c1371e267e
Nabil080 <bellilanabil8@gmail.com>
2024-09-22 01:08:34 +0200

    ex00 for loops'

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -3,21 +3,14 @@
 
 int main(int argc, char **argv)
 {
-	std::size_t	i;
-
 	if (argc < 2)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
 	{
-		argv++;
-		while (*argv)
-		{
-			i = 0;
-			while ((*argv)[i])
-				std::cout << (char)toupper((*argv)[i++]);
-			argv++;
-		}
-		std::cout << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (1);	
 	}
+	for (int i = 1; i < argc; i++)
+		for (int j = 0; argv[i][j]; j++)
+			std::cout << (char)toupper(argv[i][j]);
+	std::cout << std::endl;
 	return (0);
 }
