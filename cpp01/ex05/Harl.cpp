a6c413c32f03760a136d911538b73beb75b802e9
nbellila <nbellila@student.42.fr>
2024-09-27 16:39:33 +0200

    cpp01ex05

---
+++ cpp01/ex05/Harl.cpp
@@ -0,0 +1,42 @@
+#include "Harl.hpp"
+#include <iostream>
+
+Harl::Harl(void)
+{
+    size_t i = 0;
+    this->messages_str[i] = "DEBUG";
+    this->messages_fn[i++] = &Harl::debug;
+    this->messages_str[i] = "INFO";
+    this->messages_fn[i++] = &Harl::info;
+    this->messages_str[i] = "WARNING";
+    this->messages_fn[i++] = &Harl::warning;
+    this->messages_str[i] = "ERROR";
+    this->messages_fn[i++] = &Harl::error;
+}
+
+void    Harl::complain(std::string level)
+{
+    for (size_t i = 0; i < this->fn_count; i++)
+        if (this->messages_str[i] == level)
+            (this->*messages_fn[i])();
+}
+
+void    Harl::debug(void)
+{
+    std::cout << "[DEBUG]" << std::endl;
+}
+
+void    Harl::info(void)
+{
+    std::cout << "[INFO]" << std::endl;
+}
+
+void    Harl::warning(void)
+{
+    std::cout << "[WARNING]" << std::endl;
+}
+
+void    Harl::error(void)
+{
+    std::cout << "[ERROR]" << std::endl;
+}
