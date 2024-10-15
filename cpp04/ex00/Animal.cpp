67e968813a886fe6739ebf97875ec97c9300f469
Nabil080 <nbellila@student.42.fr>
2024-10-15 15:33:15 +0200

    cpp04 ex00 base done

---
+++ cpp04/ex00/Animal.cpp
@@ -0,0 +1,40 @@
+#include "Animal.hpp"
+#include <iostream>
+
+// canon
+
+Animal::Animal()
+{
+	std::cout << "[Animal default constructor]" << std::endl;
+	this->type = "Animal";
+}
+
+Animal::Animal(const Animal &other)
+{
+	std::cout << "[Animal copy constructor]" << std::endl;
+	*this = other;
+}
+
+Animal::~Animal()
+{
+	std::cout << "[Animal destructor]" << std::endl;
+}
+
+Animal	&Animal::operator=(const Animal &other)
+{
+	std::cout << "[Animal copy operator]" << std::endl;
+	this->type = other.type;
+	return (*this);
+}
+
+// methods
+
+std::string	Animal::getType(void) const
+{
+	return (this->type);
+}
+
+void	Animal::makeSound(void) const
+{
+	std::cout << "Random animal sound" << std::endl;
+}
