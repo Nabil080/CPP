7c325ecd2fba824d054c747ed8d43952d74c5c94
Nabil080 <bellilanabil8@gmail.com>
2024-10-16 13:30:16 +0200

    Cure & Ice

---
+++ cpp04/ex03/Cure.cpp
@@ -0,0 +1,41 @@
+#include "Cure.hpp"
+#include "ICharacter.hpp"
+
+#include <iostream>
+
+// constructors
+
+Cure::Cure()
+{
+	std::cerr << "[Cure Default Constructor called]" << std::endl;
+	this->type = "cure";
+}
+
+Cure::Cure(const Cure &other)
+{
+	std::cerr << "[Cure Copy Constructor called]" << std::endl;
+	*this = other;
+}
+
+Cure::~Cure()
+{
+	std::cerr << "[Cure Destructor called]" << std::endl;
+}
+Cure	&Cure::operator=(const Cure &other)
+{
+	if (other.type == "cure")
+		*this = other;
+	return (*this);
+}
+
+// methods
+
+void	Cure::use(ICharacter &character)
+{
+	std::cout << "* heals " + character.getName() + "'s wounds *" << std::endl;
+}
+
+AMateria*	Cure::clone() const
+{
+	return (new Cure());
+}
---
+++ cpp04/ex03/Ice.cpp
@@ -0,0 +1,41 @@
+#include "Ice.hpp"
+#include "ICharacter.hpp"
+
+#include <iostream>
+
+// constructors
+
+Ice::Ice()
+{
+	std::cerr << "[Ice Default Constructor called]" << std::endl;
+	this->type = "ice";
+}
+
+Ice::Ice(const Ice &other)
+{
+	std::cerr << "[Ice Copy Constructor called]" << std::endl;
+	*this = other;
+}
+
+Ice::~Ice()
+{
+	std::cerr << "[Ice Destructor called]" << std::endl;
+}
+Ice	&Ice::operator=(const Ice &other)
+{
+	if (other.type == "ice")
+		*this = other;
+	return (*this);
+}
+
+// methods
+
+void	Ice::use(ICharacter &character)
+{
+	std::cout << "* shoots an ice bolt at " + character.getName() + " *" << std::endl;
+}
+
+AMateria*	Ice::clone() const
+{
+	return (new Ice());
+}
