c407ffe17908c25fc71b6aa046d1c22ab5342dc4
Nabil080 <bellilanabil8@gmail.com>
2025-01-09 15:19:56 +0100

    setup with every form

---
+++ cpp05/ex02/includes/PresidentialPardonForm.hpp
@@ -0,0 +1,24 @@
+#ifndef PRESIDENTIALPARDONFORM_HPP
+#define PRESIDENTIALPARDONFORM_HPP
+
+#include "AForm.hpp"
+#include "Bureaucrat.hpp"
+class PresidentialPardonForm : public AForm
+{
+  public:
+	static const int sign_grade = 25;
+	static const int exec_grade = 5;
+
+  private:
+	std::string target;
+
+  public:
+	PresidentialPardonForm();
+	PresidentialPardonForm(PresidentialPardonForm const &other);
+	PresidentialPardonForm(std::string target);
+	~PresidentialPardonForm();
+
+	void execute(Bureaucrat const &executor) const;
+};
+
+#endif // !PRESIDENTIALPARDONFORM_HPP
