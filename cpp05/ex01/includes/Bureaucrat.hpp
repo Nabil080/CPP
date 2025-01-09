2396838c9321bee1e926f1f61f002ed0cf5b6b6b
Nabil080 <bellilanabil8@gmail.com>
2025-01-09 10:31:14 +0100

    ex01 testing done

--- cpp05/ex01/includes/Bureaucrat.hpp
+++ cpp05/ex01/includes/Bureaucrat.hpp
@@ -5,48 +5,54 @@
 #include <ostream>
 #include <string>
 
-class Bureaucrat {
-public:
-  static const int highest_grade = 1;
-  static const int lowest_grade = 150;
+class Form;
 
-private:
-  const std::string name;
-  int grade;
+class Bureaucrat
+{
+  public:
+	static const int highest_grade = 1;
+	static const int lowest_grade = 150;
 
-public:
-  Bureaucrat();
-  Bureaucrat(std::string name);
-  Bureaucrat(std::string name, int grade);
-  Bureaucrat(const Bureaucrat &other);
-  ~Bureaucrat();
-
-public:
-  Bureaucrat &operator=(const Bureaucrat &other);
-
-public:
-  void upgrade();
-  void downgrade();
-
-public:
-  std::string getName() const;
-  int getGrade() const;
-
-  class GradeTooHighException : public std::exception {
   private:
-    static const std::string message;
+	const std::string name;
+	int grade;
 
   public:
-    virtual const char *what() const throw();
-  };
-
-  class GradeTooLowException : public std::exception {
-  private:
-    static const std::string message;
+	Bureaucrat();
+	Bureaucrat(std::string name);
+	Bureaucrat(std::string name, int grade);
+	Bureaucrat(const Bureaucrat &other);
+	~Bureaucrat();
 
   public:
-    virtual const char *what() const throw();
-  };
+	Bureaucrat &operator=(const Bureaucrat &other);
+
+  public:
+	void upgrade();
+	void downgrade();
+	void tryAndSignForm(Form &form);
+
+  public:
+	std::string getName() const;
+	int getGrade() const;
+
+	class GradeTooHighException : public std::exception
+	{
+	  private:
+		static const std::string message;
+
+	  public:
+		virtual const char *what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+	  private:
+		static const std::string message;
+
+	  public:
+		virtual const char *what() const throw();
+	};
 };
 
 std::ostream &operator<<(std::ostream &out, const Bureaucrat &other);
--- cpp05/ex01/srcs/main.cpp
+++ cpp05/ex01/srcs/main.cpp
@@ -1,28 +1,63 @@
+#include "Bureaucrat.hpp"
 #include "Form.hpp"
 #include <iostream>
 
-void base_test() {
-
-  try {
-    Form base;
-    std::cout << base << std::endl;
-  } catch (std::exception &e) {
-    std::cout << e.what() << std::endl;
-  }
-
-  try {
-    Form too_low("Too low", 500, 500);
-    std::cout << too_low << std::endl;
-  } catch (std::exception &e) {
-    std::cout << e.what() << std::endl;
-  }
+void callTest(std::string name, void (*f)())
+{
+	std::cout << "--- " << name << " test ---" << std::endl;
+	{
+		f();
+	}
+	std::cout << "--- END ---" << std::endl;
 }
 
-void exception_test() {}
+namespace tests
+{
 
-int main(void) {
+void initialize_test()
+{
+	Form base("base_form");
+	std::cout << base << std::endl;
 
-  base_test();
-  exception_test();
-  return 0;
+	try
+	{
+		Form too_low("Too low", 500, 500);
+		std::cout << too_low << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	try
+	{
+		Form too_high("Too high", 0, 0);
+		std::cout << too_high << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
+
+void sign_test()
+{
+	Form boss_form("boss_form", 1, 1);
+	Form noob_form("noob_form", 150, 150);
+
+	Bureaucrat boss("boss", 1);
+	Bureaucrat noob("noob", 150);
+
+	noob.tryAndSignForm(boss_form);
+	boss.tryAndSignForm(boss_form);
+	noob_form.beSigned(noob);
+	std::cout << noob_form << std::endl;
+}
+
+} // namespace tests
+
+int main(void)
+{
+	callTest("Initialize", tests::initialize_test);
+	callTest("Sign", tests::sign_test);
+	return 0;
 }
