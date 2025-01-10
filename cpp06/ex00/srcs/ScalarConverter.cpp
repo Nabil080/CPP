f8efef13a5d1a673c46d576a78f4d875b36a00e3
Nabil080 <bellilanabil8@gmail.com>
2025-01-10 17:34:17 +0100

    getType done

--- cpp06/ex00/srcs/ScalarConverter.cpp
+++ cpp06/ex00/srcs/ScalarConverter.cpp
@@ -4,31 +4,25 @@
 #include <iostream>
 #include <string>
 
-static bool isChar(std::string literal)
+static t_type getType(std::string literal)
 {
-	if (literal.length() != 3)
-		return (false);
-	if (literal[0] != '\'' || literal[2] != '\'')
-		return (false);
-	if (!isascii(literal[1]))
-		return (false);
-	return (true);
-}
-
-static bool isInt(std::string literal)
-{
+	if (literal.length() == 3 && literal[0] == '\'' && literal[2] == '\'' && isascii(literal[1]))
+		return (CHAR);
 	std::string::const_iterator it = literal.begin();
+
 	while (it != literal.end() && std::isdigit(*it))
 		it++;
-	return (it == literal.end());
-}
-
-static t_type detectType(std::string literal)
-{
-	if (isChar(literal))
-		return (CHAR);
-	if (isInt(literal))
+	if (it == literal.end())
 		return (INT);
+	if (*it != '.')
+		return (NONE);
+	it++;
+	while (it != literal.end() && std::isdigit(*it))
+		it++;
+	if (it == literal.end())
+		return (DOUBLE);
+	if (*it == 'f' && it + 1 == literal.end())
+		return (FLOAT);
 	return (NONE);
 }
 
@@ -37,8 +31,17 @@ void ScalarConverter::convert(std::string literal)
 	if (literal.empty())
 		return;
 	// TODO: detect the type
-	t_type type = detectType(literal);
-	std::cout << type;
+	t_type type = getType(literal);
+	if (type == CHAR)
+		std::cout << "CHAR" << std::endl;
+	if (type == INT)
+		std::cout << "INT" << std::endl;
+	if (type == FLOAT)
+		std::cout << "FLOAT" << std::endl;
+	if (type == DOUBLE)
+		std::cout << "DOUBLE" << std::endl;
+	if (type == NONE)
+		std::cout << "NONE" << std::endl;
 	// TODO: convert to the type
 	// TODO: explicit conversion to other types, if overflow or impossible display error
 	// TODO: display result
