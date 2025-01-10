48975e572492d0d3594e8bd101e37a1ade091c19
Nabil080 <bellilanabil8@gmail.com>
2025-01-10 19:13:32 +0100

    converting str to type and checking for long double overflow

--- cpp06/ex00/srcs/ScalarConverter.cpp
+++ cpp06/ex00/srcs/ScalarConverter.cpp
@@ -1,7 +1,11 @@
 #include "ScalarConverter.hpp"
 #include <cctype>
+#include <cerrno>
+#include <cmath>
 #include <cstdlib>
+#include <iomanip>
 #include <iostream>
+#include <limits>
 #include <string>
 
 static t_type getType(std::string literal)
@@ -26,26 +30,34 @@ static t_type getType(std::string literal)
 	return (NONE);
 }
 
-void ScalarConverter::convert(std::string literal)
+static void printNumbers(long double number)
 {
-	if (literal.empty())
-		return;
+	std::cout << "Number: " << number << std::endl;
+	std::cout << "Errno: " << errno << std::endl;
+}
+
+int ScalarConverter::convert(std::string literal)
+{
+	std::cerr << std::fixed << std::setprecision(1);
+	if (literal.empty() || std::isfinite(std::strtold(literal.c_str(), NULL)) == 0)
+		return (0);
 	// TODO: detect the type
 	t_type type = getType(literal);
-	if (type == CHAR)
-		std::cout << "CHAR" << std::endl;
-	if (type == INT)
-		std::cout << "INT" << std::endl;
-	if (type == FLOAT)
-		std::cout << "FLOAT" << std::endl;
-	if (type == DOUBLE)
-		std::cout << "DOUBLE" << std::endl;
 	if (type == NONE)
-		std::cout << "NONE" << std::endl;
+		return (0);
 	// TODO: convert to the type
+	long double number = 0;
+	if (type == CHAR)
+		number = static_cast<char>(literal[1]);
+	if (type == INT)
+		number = atoi(literal.c_str());
+	if (type == FLOAT)
+		number = std::strtof(literal.c_str(), NULL);
+	if (type == DOUBLE)
+		number = std::strtod(literal.c_str(), NULL);
 	// TODO: explicit conversion to other types, if overflow or impossible display error
 	// TODO: display result
+	printNumbers(number);
 
-	(void)type;
-	(void)literal;
+	return (1);
 }
--- cpp06/ex00/srcs/main.cpp
+++ cpp06/ex00/srcs/main.cpp
@@ -1,9 +1,12 @@
 #include "ScalarConverter.hpp"
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
+	if (argc != 2 || ScalarConverter::convert(argv[1]) == 0)
+	{
+		std::cout << "Please give a single valid literal as an argument" << std::endl;
 		return (1);
-	ScalarConverter::convert(argv[1]);
+	}
 	return (0);
 }
