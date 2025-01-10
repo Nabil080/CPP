52d96c39629d5e0fa0f57b638697f902826763da
Nabil080 <bellilanabil8@gmail.com>
2025-01-10 20:05:52 +0100

    cpp06 ex01 done

--- cpp06/ex00/includes/ScalarConverter.hpp
+++ cpp06/ex00/includes/ScalarConverter.hpp
@@ -20,6 +20,14 @@ class ScalarConverter
 
   public:
 	static int convert(std::string literal);
+
+  private: // helpers
+	static t_type getType(std::string literal);
+	static void printNumbers(long double converted_number, long double ld_number);
+	static void printDouble(double nb, long double ld_number);
+	static void printFloat(float nb, long double ld_number);
+	static void printInt(int nb, long double ld_number);
+	static void printChar(char c, long double ld_number);
 };
 
 #endif // !SCALARCONVERTER_HPP
--- cpp06/ex00/srcs/ScalarConverter.cpp
+++ cpp06/ex00/srcs/ScalarConverter.cpp
@@ -8,12 +8,14 @@
 #include <limits>
 #include <string>
 
-static t_type getType(std::string literal)
+t_type ScalarConverter::getType(std::string literal)
 {
 	if (literal.length() == 3 && literal[0] == '\'' && literal[2] == '\'' && isascii(literal[1]))
 		return (CHAR);
 	std::string::const_iterator it = literal.begin();
 
+	if (*it == '+' || *it == '-')
+		it++;
 	while (it != literal.end() && std::isdigit(*it))
 		it++;
 	if (it == literal.end())
@@ -30,34 +32,74 @@ static t_type getType(std::string literal)
 	return (NONE);
 }
 
-static void printNumbers(long double number)
+void ScalarConverter::printChar(char c, long double ld_number)
 {
-	std::cout << "Number: " << number << std::endl;
-	std::cout << "Errno: " << errno << std::endl;
+	if (ld_number < std::numeric_limits<char>::min() || ld_number > std::numeric_limits<char>::max())
+		std::cout << "char: Overflow\n";
+	else if (std::isprint(c) == 0)
+		std::cout << "char: Non displayable\n";
+	else
+		std::cout << "char: '" << c << "'\n";
+}
+
+void ScalarConverter::printInt(int nb, long double ld_number)
+{
+	if (ld_number < std::numeric_limits<int>::min() || ld_number > std::numeric_limits<int>::max())
+		std::cout << "int: Overflow\n";
+	else
+		std::cout << "int: " << nb << "\n";
+}
+
+void ScalarConverter::printFloat(float nb, long double ld_number)
+{
+	std::cout << std::fixed << std::setprecision(1);
+	if (ld_number < -std::numeric_limits<float>::max() || ld_number > std::numeric_limits<float>::max())
+		std::cout << "float: Overflow\n";
+	else
+		std::cout << "float: " << nb << "f\n";
+}
+
+void ScalarConverter::printDouble(double nb, long double ld_number)
+{
+	std::cout << std::fixed << std::setprecision(1);
+	if (ld_number < -std::numeric_limits<double>::max() || ld_number > std::numeric_limits<double>::max())
+		std::cout << "double: Overflow\n";
+	else
+		std::cout << "double: " << nb << "\n";
+}
+
+void ScalarConverter::printNumbers(long double converted_number, long double ld_number)
+{
+	printChar(static_cast<char>(converted_number), ld_number);
+	printInt(static_cast<int>(converted_number), ld_number);
+	printFloat(static_cast<float>(converted_number), ld_number);
+	printDouble(static_cast<double>(converted_number), ld_number);
 }
 
 int ScalarConverter::convert(std::string literal)
 {
-	std::cerr << std::fixed << std::setprecision(1);
-	if (literal.empty() || std::isfinite(std::strtold(literal.c_str(), NULL)) == 0)
+	if (literal.empty())
 		return (0);
-	// TODO: detect the type
+	// detect the type
+	long double ld_number = std::strtold(literal.c_str(), NULL);
+	if (std::isfinite(ld_number) == 0)
+		return (0);
 	t_type type = getType(literal);
 	if (type == NONE)
 		return (0);
-	// TODO: convert to the type
-	long double number = 0;
+	// convert to the type
+	long double converted_number = 0;
 	if (type == CHAR)
-		number = static_cast<char>(literal[1]);
+		converted_number = static_cast<char>(literal[1]);
 	if (type == INT)
-		number = atoi(literal.c_str());
+		converted_number = atoi(literal.c_str());
 	if (type == FLOAT)
-		number = std::strtof(literal.c_str(), NULL);
+		converted_number = std::strtof(literal.c_str(), NULL);
 	if (type == DOUBLE)
-		number = std::strtod(literal.c_str(), NULL);
+		converted_number = std::strtod(literal.c_str(), NULL);
 	// TODO: explicit conversion to other types, if overflow or impossible display error
 	// TODO: display result
-	printNumbers(number);
+	printNumbers(converted_number, ld_number);
 
 	return (1);
 }
