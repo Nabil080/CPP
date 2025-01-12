663dc49908488cbda45610bb86d1d9cb2fce74b9
Nabil080 <bellilanabil8@gmail.com>
2025-01-12 20:36:18 +0100

    save

--- cpp08/ex00/srcs/main.cpp
+++ cpp08/ex00/srcs/main.cpp
@@ -3,13 +3,15 @@
 #include <iostream>
 #include <vector>
 
-int main(void)
+static void vector_test()
 {
 	std::vector<int> array;
 
 	array.reserve(100);
 	for (size_t i = 0; i < 100; i++)
 		array.push_back(i);
+
+	std::cout << "Vector content : ";
 	for (std::vector<int>::iterator it = array.begin(); it < array.end(); it++)
 		std::cout << *it << " ";
 	std::cout << std::endl;
@@ -17,6 +19,11 @@ int main(void)
 	test_easyfind(array, 5);
 	test_easyfind(array, 50);
 	test_easyfind(array, 500);
+}
+
+int main(void)
+{
+	vector_test();
 
 	return (0);
 }
