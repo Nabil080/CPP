ee3b80d2747753d2e764b9903683e682286ebc42
Nabil080 <bellilanabil8@gmail.com>
2025-01-29 17:40:43 +0100

    fix cpp01ex02, pointer adress and value

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -1,18 +1,18 @@
-#include <string>
 #include <iostream>
+#include <string>
 
 int main(void)
 {
-    std::string     str = "HI THIS IS BRAIN";
-    std::string     *stringPTR = &str;
-    std::string&    stringREF = str;
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
 
-    std::cout << "str adress : " << &str << std::endl;
-    std::cout << "pointer adress : " << &stringPTR << std::endl;
-    std::cout << "reference adress : " << &stringREF << std::endl;
-    std::cout << "--------------------------" << std::endl;
-    std::cout << "str value : " << str << std::endl;
-    std::cout << "pointer value : " << stringPTR << std::endl;
-    std::cout << "reference value : " << stringREF << std::endl;
-    return (0);
+	std::cout << "str adress : " << &str << std::endl;
+	std::cout << "pointer adress : " << stringPTR << std::endl;
+	std::cout << "reference adress : " << &stringREF << std::endl;
+	std::cout << "--------------------------" << std::endl;
+	std::cout << "str value : " << str << std::endl;
+	std::cout << "pointer value : " << *stringPTR << std::endl;
+	std::cout << "reference value : " << stringREF << std::endl;
+	return (0);
 }
