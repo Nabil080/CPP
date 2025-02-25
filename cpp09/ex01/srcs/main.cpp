4e0c27f6884af8ec7f404d3e22deabae63a7e75a
Nabil080 <bellilanabil8@gmail.com>
2025-02-25 16:49:43 +0100

    format

--- cpp09/ex01/srcs/main.cpp
+++ cpp09/ex01/srcs/main.cpp
@@ -8,11 +8,16 @@
 static void defaultTests()
 {
 	RPN_Test tests[] = {
-		RPN_Test("expression", ERR_EXPRESSION),	   RPN_Test("10 20 +", ERR_EXPRESSION),
-		RPN_Test("1 2+3+", ERR_EXPRESSION),		   RPN_Test("1 2 + * 3 4 +", ERR_TOOMANY_OPERATORS),
-		RPN_Test("1 + 2", ERR_TOOMANY_OPERATORS),  RPN_Test("1 2 3 + 4 5 -", ERR_TOOLITTLE_OPERATORS),
-		RPN_Test("8 9 * 9 - 9 - 9 - 4 - 1 +", 42), RPN_Test("7 7 * 7 -", 42),
-		RPN_Test("1 2 * 2 / 2 * 2 4 - +", 0),	   RPN_Test("(1 + 1)", ERR_EXPRESSION),
+		RPN_Test("expression", ERR_EXPRESSION),
+		RPN_Test("10 20 +", ERR_EXPRESSION),
+		RPN_Test("1 2+3+", ERR_EXPRESSION),
+		RPN_Test("1 2 + * 3 4 +", ERR_TOOMANY_OPERATORS),
+		RPN_Test("1 + 2", ERR_TOOMANY_OPERATORS),
+		RPN_Test("1 2 3 + 4 5 -", ERR_TOOLITTLE_OPERATORS),
+		RPN_Test("8 9 * 9 - 9 - 9 - 4 - 1 +", 42),
+		RPN_Test("7 7 * 7 -", 42),
+		RPN_Test("1 2 * 2 / 2 * 2 4 - +", 0),
+		RPN_Test("(1 + 1)", ERR_EXPRESSION),
 	};
 
 	for (size_t i = 0; i < (sizeof(tests) / sizeof(RPN_Test)); i++)
@@ -35,16 +40,18 @@ static int printExpressionResult(std::string expression)
 		std::cerr << "Error: " << e.what() << std::endl;
 		return (1);
 	}
-    return (0);
+	return (0);
 }
 
 int main(int argc, char **argv)
 {
 	if (argc != 2 || argv[1][0] == '\0')
 	{
-		std::cerr << "This program takes an inverted Polish mathematical expression as an argument" << std::endl;
-        if (argc != 1)
-            return (1);
+		std::cerr << "This program takes an inverted Polish mathematical "
+					 "expression as an argument"
+				  << std::endl;
+		if (argc != 1)
+			return (1);
 		std::cout << "No argument given, using default tests" << std::endl << std::endl;
 		defaultTests();
 		return (0);
